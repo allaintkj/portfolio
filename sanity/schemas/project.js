ed91e5f6fe4ee26fdf55582e31a6208bdf98f658
Tom Allain <tom.allain@protonmail.com>
2020-12-15 18:30:23 -0400

    implement sanity.io and initial schema;

---
+++ sanity/schemas/project.js
@@ -0,0 +1,65 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'projectType',
+      title: 'Project Type',
+      type: 'string',
+      options: {
+        list: [
+          { value: 'personal', title: 'Personal' },
+          { value: 'school', title: 'School' }
+        ]
+      }
+    },
+    {
+      name: 'link',
+      type: 'url',
+    },
+    {
+      name: 'techs',
+      type: 'array',
+      of: [{
+        type: 'string'
+      }],
+      options: {
+        layout: 'tags'
+      }
+    }
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+    }
+  }
+}
